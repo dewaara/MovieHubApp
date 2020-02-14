e4625d14508a58e7e40b126407bf7ce49cb31222
dewaara <mdh03263@gmail.com>
2020-02-15 00:01:04 +0530

    The 1st Part of #MovieHub Developer by:- Md.Halim

---
+++ MovieHub_Part_1/app/src/main/java/com/dewaara/moviehub/HomeActivity.java
@@ -0,0 +1,43 @@
+package com.dewaara.moviehub;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private List<Slide> lstSlides;
+    private ViewPager sliderpager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        sliderpager = findViewById(R.id.slider_pager);
+
+        lstSlides = new ArrayList<>();
+        lstSlides.add(new Slide(R.drawable.slide1,"Slide Title \nmore text here"));
+        lstSlides.add(new Slide(R.drawable.slide2,"Slide Title \nmore text here"));
+        lstSlides.add(new Slide(R.drawable.slide1,"Slide Title \nmore text here"));
+        lstSlides.add(new Slide(R.drawable.slide2,"Slide Title \nmore text here"));
+        lstSlides.add(new Slide(R.drawable.slide1,"Slide Title \nmore text here"));
+        lstSlides.add(new Slide(R.drawable.slide2,"Slide Title \nmore text here"));
+        lstSlides.add(new Slide(R.drawable.slide1,"Slide Title \nmore text here"));
+        lstSlides.add(new Slide(R.drawable.slide2,"Slide Title \nmore text here"));
+        lstSlides.add(new Slide(R.drawable.slide1,"Slide Title \nmore text here"));
+        lstSlides.add(new Slide(R.drawable.slide2,"Slide Title \nmore text here"));
+        lstSlides.add(new Slide(R.drawable.slide1,"Slide Title \nmore text here"));
+        lstSlides.add(new Slide(R.drawable.slide2,"Slide Title \nmore text here"));
+
+
+        SliderPagerAdapter adapter = new SliderPagerAdapter(this,lstSlides);
+
+        sliderpager.setAdapter(adapter);
+
+    }
+}
